99f612bfe32865521aa61cecafe4832dff01fbb9
Tomislav Ficko <ficko.tomislav@gmail.com>
2020-03-28 18:12:20 +0100

    Connect storyboard components to ViewController

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -2,19 +2,64 @@
 //  ViewController.swift
 //  Calculator Layout iOS13
 //
-//  Created by Angela Yu on 01/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Tomislav Ficko on 28/03/2020.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var entryField: UILabel!
+    
+    var currentState: Float = 0
+    var currentString: String = "0"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func deleteSwipe(_ sender: UISwipeGestureRecognizer) {
+    }
+    @IBAction func percentButton(_ sender: UIButton) {
+    }
+    @IBAction func posNegButton(_ sender: UIButton) {
+    }
+    @IBAction func clearButton(_ sender: UIButton) {
+    }
+    @IBAction func divideButton(_ sender: UIButton) {
+    }
+    @IBAction func multiplyButton(_ sender: UIButton) {
+    }
+    @IBAction func subtractButton(_ sender: UIButton) {
+    }
+    @IBAction func addButton(_ sender: UIButton) {
+    }
+    @IBAction func resultButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func commaButton(_ sender: UIButton) {
+    }
+    @IBAction func zeroButton(_ sender: UIButton) {
+    }
+    @IBAction func oneButton(_ sender: UIButton) {
+    }
+    @IBAction func twoButton(_ sender: UIButton) {
+    }
+    @IBAction func threeButton(_ sender: UIButton) {
+    }
+    @IBAction func fourButton(_ sender: UIButton) {
+    }
+    @IBAction func fiveButton(_ sender: UIButton) {
+    }
+    @IBAction func sixButton(_ sender: UIButton) {
+    }
+    @IBAction func sevenButton(_ sender: UIButton) {
+    }
+    @IBAction func eightButton(_ sender: UIButton) {
+    }
+    @IBAction func nineButton(_ sender: UIButton) {
+    }
+    
 }
-
